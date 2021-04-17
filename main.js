30df608bcb8fa5e272b12b920d1d8f5ebbe5e85c
Webkenstein <peter.odalos@gmail.com>
2021-04-18 00:45:53 +0200

    5 pomylena na 6

--- main.js
+++ main.js
@@ -6,10 +6,24 @@ const app = Vue.createApp({
             image: './assets/images/socks_blue.jpg',
             inStock: true,
             details: ['50% cotton', '30% wool', '20% polyester'],
+<<<<<<< Updated upstream
             variants: [
               { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
               { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
             ]
+=======
+            todos: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                {id: 1234, farba: 'Hnedá'},
+                {id: 4567, farba: 'fialová'}
+            ],
+            sizes: [
+               'XS', 'S', 'M', 'L', 'XL', 'XXL'
+            ]
+
+
+>>>>>>> Stashed changes
         }
     }
 })
+
